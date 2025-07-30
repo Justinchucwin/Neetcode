9011c6554073dc1de6ea477b94e5c13c9a36e564
Bustindwin <justinchucwin@gmail.com>
2025-07-29 22:33:51 -0700

    Neetcode day 2

--- neetcode150.py
+++ neetcode150.py
@@ -1,3 +1,8 @@
+class listnode:
+    def __init__(self,value=0,next=None):
+        self.value=value
+        self.next=next
+
 def binarysearch(array,l,r,target):
     if r<l:
         return -1
@@ -9,6 +14,7 @@ def binarysearch(array,l,r,target):
             return binarysearch(array,l,mid-1,target)
         else:
             return binarysearch(array,mid+1,r,target)
+
 
 def maxProfit(prices):#index is day, elements are price
     l,r=0,1
@@ -23,6 +29,7 @@ def maxProfit(prices):#index is day, elements are price
         r+=1
     return maxp
 
+
 def isAnagram(s,t):
     counts,countt={},{}
     if len(s)!=len(t):
@@ -33,10 +40,79 @@ def isAnagram(s,t):
     return counts==countt
 
 
+def scoreofstring(s):
+    sum=0
+    lengthstr=len(s)
+    for i in range(lengthstr//2):
+        sum+=abs(ord(s[i])-ord(s[i+1]))
+        if lengthstr%2==0 and (lengthstr//2)==i:
+            break
+        endpos=lengthstr-1-i 
+        sum+=abs(ord(s[endpos])-ord(s[endpos-1]))
+    return sum
+
+
+def validpalindrome(s):
+    startind=0
+    endind=len(s)-1
+    while endind>startind:
+        if s[startind].lower()==s[endind].lower():
+            startind+=1
+            endind-=1
+        else:
+            if s[startind].isalnum() and s[endind].isalnum():
+                return False
+            else:
+                if not s[startind].isalnum():
+                    startind+=1
+                if not s[endind].isalnum():
+                    endind-=1     
+    return True
+
+
+def isValid(s):#gets a string as an arguement and see's if it follows rule of closed parentheses
+    stack=[]
+    closetoopen={")":"(","}":"{","[":"]"}
+
+    for i in s:
+        if i in closetoopen:
+            if stack and closetoopen[i]==stack[-1]:#means stack is not empty
+                stack.pop()
+            else:
+                return False
+        else:
+            stack.append(i)
+    if not stack:
+        return True
+    return False
+
+
+def reverselist(head):
+    prev,curr=None,head
+
+    while curr:
+        temp=curr.next
+        curr.next=prev
+        prev=curr
+        curr=temp
+    return prev
+            
+
+            
+
+
 def main():
     #print(binarysearch([1,2,3,4,5,6],0,5,4))
     #print(maxProfit([10,8,7,5,2]))
-    print(isAnagram("racecar","carrace"))
+    #print(isAnagram("racecar","carrace"))
+    head=listnode(0)
+    current=head
+    for i in range(3):
+        ln=listnode(i+1)
+        current.next=ln
+        current=current.next
+
+       
 
 
 main()
